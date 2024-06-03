fe6e7b8da2b60b4979b8f76a50575005c76befbe
Alexandre <103534039+AlexSakai@users.noreply.github.com>
2024-06-03 12:33:34 -0300

    Add files via upload

--- script/estudo.js
+++ script/estudo.js
@@ -3,3 +3,10 @@ const btnTeste = document.querySelector("#teste");
 btnTeste.onclick = () => {
     alert("Funcionou!");
 }
+
+fazerLogin(){
+    const usuario = document.getElementById("usuario");
+    const senha = document.getElementById("senha");
+
+    
+}
